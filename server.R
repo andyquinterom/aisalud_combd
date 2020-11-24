97cf21ae79f203e085d1a9b4fea87fbb21e41d92
andyquinterom <andyquinterom@users.noreply.github.com>
2020-11-24 11:27:57 -0500

    Modulo para cargar datos

--- server.R
+++ server.R
@@ -1,3 +1,12 @@
 shinyServer(function(input, output, session) {
   
+  # Modulo para cargar datps
+  
+  datos <- callModule(
+    module = prepara_server,
+    id = "cargar_datos",
+    nombre_id = "cargar_datos"
+  )
+  
+  
 })
--- ui.R
+++ ui.R
@@ -1,3 +1,10 @@
 shinyUI(function(request) {tagList(
-  
+  fluidPage(
+    sidebarLayout(
+      sidebarPanel = sidebarPanel(
+        prepara_ui("cargar_datos")
+      ),
+      mainPanel = mainPanel()
+    )
+  )
 )})
